d105a5ffb453575c189b6f8bab28c780882c2a30
Goberman15 <akbarrmdhn94@gmail.com>
2023-12-07 22:07:20 +0700

    Setup Backend

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.29.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "remote-state-tf-1523"
+    key            = "project/serverless-web-backend"
+    region         = "ap-southeast-1"
+    dynamodb_table = "tf-locks-table"
+    encrypt        = true
+  }
+}
