34cfc88565cea3d2f4d954180097ca419bf07471
Goberman15 <akbarrmdhn94@gmail.com>
2023-12-09 06:06:23 +0700

    Create Access Logging for Dev Stage

--- apigw.tf
+++ apigw.tf
@@ -13,6 +13,22 @@ resource "aws_apigatewayv2_route" "post_order" {
 resource "aws_apigatewayv2_stage" "dev" {
   api_id = aws_apigatewayv2_api.apigw.id
   name   = "dev"
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigw_dev_log_group.arn
+    format = jsonencode({
+      requestId        = "$context.requestId"
+      ip               = "$context.identity.sourceIp"
+      requestTime      = "$context.requestTime"
+      httpMethod       = "$context.httpMethod"
+      routeKey         = "$context.routeKey"
+      status           = "$context.status"
+      protocol         = "$context.protocol"
+      responseLength   = "$context.responseLength"
+      integrationError = "$context.integrationErrorMessage"
+    })
+  }
+
+  tags = local.tags
 }
 
 resource "aws_apigatewayv2_integration" "sqs_integration" {
