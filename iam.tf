f3135374cdf912a1b5b1b77aa33c6690262d5447
Goberman15 <akbarrmdhn94@gmail.com>
2023-12-08 22:53:53 +0700

    Update Role for API GW

--- iam.tf
+++ iam.tf
@@ -74,7 +74,23 @@ data "aws_iam_policy_document" "sqs_read_policy_document" {
     ]
     resources = ["*"]
   }
+}
 
+resource "aws_iam_policy" "sqs_send_message_policy" {
+  name_prefix = "sqs_send_message_policy"
+  policy = data.aws_iam_policy_document.sqs_send_message_policy_document.json
+  tags        = local.tags
+}
+
+data "aws_iam_policy_document" "sqs_send_message_policy_document" {
+  statement {
+    sid    = "SQSSendPolicy081223"
+    effect = "Allow"
+    actions = [
+      "SQS:SendMessage"
+    ]
+    resources = ["*"]
+  }
 }
 
 data "aws_iam_policy" "lambda_basic_execution_role" {
@@ -86,7 +102,7 @@ data "aws_iam_policy" "apigw_push_cw_logs" {
 }
 
 resource "aws_iam_role" "lambda_sqs_ddb" {
-  name_prefix = "lambda_sqs_ddb_role"
+  name_prefix        = "lambda_sqs_ddb_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy_document.json
   managed_policy_arns = [
     aws_iam_policy.sqs_read_policy.arn,
@@ -97,7 +113,7 @@ resource "aws_iam_role" "lambda_sqs_ddb" {
 }
 
 resource "aws_iam_role" "lambda_ddb_stream_sns" {
-  name_prefix = "lambda_ddb_stream_sns_role"
+  name_prefix        = "lambda_ddb_stream_sns_role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy_document.json
   managed_policy_arns = [
     aws_iam_policy.ddb_stream_read_policy.arn,
@@ -108,9 +124,10 @@ resource "aws_iam_role" "lambda_ddb_stream_sns" {
 }
 
 resource "aws_iam_role" "api_gw_sqs" {
-  name_prefix = "api_gw_sqs_role"
+  name_prefix        = "api_gw_sqs_role"
   assume_role_policy = data.aws_iam_policy_document.apigw_assume_role_policy_document.json
   managed_policy_arns = [
+    aws_iam_policy.sqs_send_message_policy.arn,
     data.aws_iam_policy.apigw_push_cw_logs.arn
   ]
   tags = local.tags
@@ -132,7 +149,7 @@ data "aws_iam_policy_document" "apigw_assume_role_policy_document" {
     effect = "Allow"
     principals {
       type        = "Service"
-      identifiers = ["ops.apigateway.amazonaws.com"]
+      identifiers = ["apigateway.amazonaws.com"]
     }
     actions = ["sts:AssumeRole"]
   }
