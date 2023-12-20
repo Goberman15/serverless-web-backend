61a69890d728b4e277a9a588b63949cf2f861137
Goberman15 <akbarrmdhn94@gmail.com>
2023-12-20 07:24:58 +0700

    Add Route for Patch Record

--- apigw.tf
+++ apigw.tf
@@ -10,6 +10,12 @@ resource "aws_apigatewayv2_route" "post_order" {
   target    = "integrations/${aws_apigatewayv2_integration.sqs_integration.id}"
 }
 
+resource "aws_apigatewayv2_route" "patch_order" {
+  api_id    = aws_apigatewayv2_api.apigw.id
+  route_key = "PATCH /order/{orderId}"
+  target    = "integrations/${aws_apigatewayv2_integration.sqs_integration_patch.id}"
+}
+
 resource "aws_apigatewayv2_stage" "dev" {
   api_id = aws_apigatewayv2_api.apigw.id
   name   = "dev"
@@ -23,8 +29,10 @@ resource "aws_apigatewayv2_stage" "dev" {
       routeKey         = "$context.routeKey"
       status           = "$context.status"
       protocol         = "$context.protocol"
+      path             = "$context.path"
       responseLength   = "$context.responseLength"
       integrationError = "$context.integrationErrorMessage"
+      errorMessage     = "$context.error.message"
     })
   }
 
@@ -40,6 +48,30 @@ resource "aws_apigatewayv2_integration" "sqs_integration" {
   request_parameters = {
     QueueUrl    = aws_sqs_queue.storage_queue.id
     MessageBody = "$request.body"
+    MessageAttributes = jsonencode({
+      action = {
+        DataType    = "String"
+        StringValue = "PlaceOrder"
+      }
+    })
+  }
+}
+
+resource "aws_apigatewayv2_integration" "sqs_integration_patch" {
+  api_id              = aws_apigatewayv2_api.apigw.id
+  integration_type    = "AWS_PROXY"
+  integration_subtype = "SQS-SendMessage"
+  credentials_arn     = aws_iam_role.api_gw_sqs.arn
+
+  request_parameters = {
+    QueueUrl    = aws_sqs_queue.storage_queue.id
+    MessageBody = "$request.path.orderId"
+    MessageAttributes = jsonencode({
+      action = {
+        DataType    = "String"
+        StringValue = "PayOrder"
+      }
+    })
   }
 }
 
